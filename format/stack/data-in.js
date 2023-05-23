c9fef2d163530520a55fd85bed9b3a4336abf85c
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2023-05-23 21:56:09 +0200

    Read xml v3 format + new build.

--- format/stack/data-in.js
+++ format/stack/data-in.js
@@ -13,78 +13,93 @@ class MetavizInStack {
 
         metaviz.editor.id = xml.querySelector('mv > id').textContent;
 
+        const version = parseInt(xml.querySelector('mv > version').textContent);
+        if (version != 3) {
+            alert('Unknown file version!');
+            return;
+        }
+
         const packets = [];
 
         const history = xml.querySelector('mv > history');
 
         for (let i = 0; i < history.children.length; i++) {
-            const element = history.children[i];
-            switch (element.tagName) {
+            const session = history.children[i];
 
-                case 'add':
-                    let packetAdd = {
-                        action: 'add',
-                        timestamp: element.getAttribute('timestamp')
-                    };
-                    if (element.getAttribute('nodes')) packetAdd['nodes'] = JSON.parse(element.getAttribute('nodes'));
-                    if (element.getAttribute('links')) packetAdd['links'] = JSON.parse(element.getAttribute('links'));
-                    packets.push(packetAdd);
-                    break;
+            for (let j = 0; j < session.children.length; j++) {
+                const element = session.children[j];
+                switch (element.tagName) {
 
-                case 'del':
-                    let packetDel = {
-                        action: 'del',
-                        timestamp: element.getAttribute('timestamp')
-                    };
-                    if (element.getAttribute('nodes')) packetDel['nodes'] = JSON.parse(element.getAttribute('nodes'));
-                    if (element.getAttribute('links')) packetDel['links'] = JSON.parse(element.getAttribute('links'));
-                    packets.push(packetDel);
-                    break;
+                    case 'add':
+                        let packetAdd = {
+                            action: 'add',
+                            timestamp: element.getAttribute('timestamp'),
+                            session: session.id
+                        };
+                        if (element.getAttribute('nodes')) packetAdd['nodes'] = JSON.parse(element.getAttribute('nodes'));
+                        if (element.getAttribute('links')) packetAdd['links'] = JSON.parse(element.getAttribute('links'));
+                        packets.push(packetAdd);
+                        break;
 
-                case 'move':
-                    let packetMove = {
-                        action: 'move',
-                        timestamp: element.getAttribute('timestamp'),
-                        nodes: element.getAttribute('nodes').split(',')
-                    };
-                    if (element.getAttribute('offsetX')) packetMove['offset'] = {
-                        x: parseInt(element.getAttribute('offsetX')),
-                        y: parseInt(element.getAttribute('offsetY'))
-                    };
-                    if (element.getAttribute('positionX')) packetMove['position'] = {
-                        x: parseInt(element.getAttribute('positionX')),
-                        y: parseInt(element.getAttribute('positionY'))
-                    };
-                    packets.push(packetMove);
-                    break;
+                    case 'del':
+                        let packetDel = {
+                            action: 'del',
+                            timestamp: element.getAttribute('timestamp'),
+                            session: session.id
+                        };
+                        if (element.getAttribute('nodes')) packetDel['nodes'] = JSON.parse(element.getAttribute('nodes'));
+                        if (element.getAttribute('links')) packetDel['links'] = JSON.parse(element.getAttribute('links'));
+                        packets.push(packetDel);
+                        break;
 
-                case 'resize':
-                    packets.push({
-                        action: 'resize',
-                        timestamp: element.getAttribute('timestamp'),
-                        nodes: element.getAttribute('nodes').split(','),
-                        size: {
-                            w: parseInt(element.getAttribute('w')),
-                            h: parseInt(element.getAttribute('h'))
+                    case 'move':
+                        let packetMove = {
+                            action: 'move',
+                            timestamp: element.getAttribute('timestamp'),
+                            session: session.id,
+                            nodes: element.getAttribute('nodes').split(',')
+                        };
+                        if (element.getAttribute('offsetX')) packetMove['offset'] = {
+                            x: parseInt(element.getAttribute('offsetX')),
+                            y: parseInt(element.getAttribute('offsetY'))
+                        };
+                        if (element.getAttribute('positionX')) packetMove['position'] = {
+                            x: parseInt(element.getAttribute('positionX')),
+                            y: parseInt(element.getAttribute('positionY'))
+                        };
+                        packets.push(packetMove);
+                        break;
+
+                    case 'resize':
+                        packets.push({
+                            action: 'resize',
+                            timestamp: element.getAttribute('timestamp'),
+                            session: session.id,
+                            nodes: element.getAttribute('nodes').split(','),
+                            size: {
+                                w: parseInt(element.getAttribute('w')),
+                                h: parseInt(element.getAttribute('h'))
+                            }
+                        });
+                        break;
+
+                    case 'param':
+                        let data = {};
+                        for (const [key, value] of Object.entries(JSON.parse(element.getAttribute('data')))) {
+                            data[key] = value.unescape();
                         }
-                    });
-                    break;
+                        packets.push({
+                            action: 'param',
+                            timestamp: element.getAttribute('timestamp'),
+                            session: session.id,
+                            node: {id: element.getAttribute('node')},
+                            data: data
+                        });
+                        break;
 
-                case 'param':
-                    let data = {};
-                    for (const [key, value] of Object.entries(JSON.parse(element.getAttribute('data')))) {
-                        data[key] = value.unescape();
-                    }
-                    packets.push({
-                        action: 'param',
-                        timestamp: element.getAttribute('timestamp'),
-                        node: {id: element.getAttribute('node')},
-                        data: data
-                    });
-                    break;
-
-            }
-        }
+                }
+            } // for j
+        } // for i
         metaviz.editor.history.set(packets.sort((a, b) => a.timestamp - b.timestamp));
         metaviz.editor.history.recreate();
     }
