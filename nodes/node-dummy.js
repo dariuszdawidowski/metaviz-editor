daa86d25c854cd4480eae0173b0cf79d7072e27b
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2023-06-09 15:16:54 +0200

    Create Node Dummy when original node not found.

--- nodes/node-dummy.js
+++ nodes/node-dummy.js
@@ -1,7 +1,22 @@
 /**
  * Metaviz Node Dummy
- * (c) 2009-2022 Dariusz Dawidowski, All Rights Reserved.
+ * (c) 2009-2023 Dariusz Dawidowski, All Rights Reserved.
  */
 
 class MetavizNodeDummy extends MetavizNode {
+
+    /**
+     * Constructor
+     */
+
+    constructor(args) {
+        super(args);
+        const info = document.createElement('div');
+        info.classList.add('info');
+        info.innerHTML = `❌<br>${'type' in args ? args.type : 'Node'}<br>NOT found<br>☣️<br>Quarantined until repaired`;
+        this.element.append(info);
+        this.setSize({width: 120, height: 120});
+        this.sockets.get = (transform) => {return {x: this.transform.x, y: this.transform.y}};
+    }
+
 }
