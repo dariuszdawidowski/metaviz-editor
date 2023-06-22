9f5039430ade0220db5466dc2f65f1e827a5d909
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2023-06-22 17:06:40 +0200

    Bump to 0.9.10

--- app/metaviz.js
+++ app/metaviz.js
@@ -148,7 +148,7 @@ class Metaviz {
     constructor() {
 
         // App version
-        this.version = '0.9.9';
+        this.version = '0.9.10';
 
         // Agent Information about client properties
         this.agent = {
