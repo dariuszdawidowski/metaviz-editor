06eea4707d8db7de36333d4e3e91ae01fcb147fe
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-09-17 10:24:15 +0200

    Set value for popover boolean

--- controls/popovers/boolean.js
+++ controls/popovers/boolean.js
@@ -19,8 +19,7 @@ class MetavizPopoverBoolean {
 
         // Icon
         this.element = document.createElement('span');
-        this.element.classList.add('toolbar-action');
-        this.element.innerHTML = '<svg style="pointer-events: none;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20" fill="none" stroke="var(--paper-2)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="20" rx="3" ry="3" stroke="var(--paper-2)" fill="none"/><path d="M5 10l3 3l7-7" /></svg>';
+        this.element.classList.add('toolbar-action', 'popover-boolean');
 
         // Cloud
         const cloud = document.createElement('div');
@@ -28,8 +27,25 @@ class MetavizPopoverBoolean {
         this.element.append(cloud);
 
         // Switcher
-        const switcher = new TotalProMenuSwitch({text, value, onChange});
+        const switcher = new TotalProMenuSwitch({text, value, onChange: (value) => {
+            this.set(value);
+            if (onChange) onChange(value);
+        }});
         cloud.append(switcher.element);
+
+        // Initial value
+        this.set(value);
+    }
+
+    /**
+     * Set appearance
+     */
+
+    set(value) {
+        if (value == true)
+            this.element.innerHTML = '<svg style="pointer-events: none;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20" fill="none" stroke="var(--paper-2)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="20" rx="3" ry="3" stroke="var(--paper-2)" fill="none"/><path d="M5 10l3 3l7-7" /></svg>';
+        else
+            this.element.innerHTML = '<svg style="pointer-events: none;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20" fill="none" stroke="var(--paper-2)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="20" rx="3" ry="3" stroke="var(--paper-2)" fill="none"/></svg>';
     }
 
 }
